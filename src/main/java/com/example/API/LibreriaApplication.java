fa2e111338699b75d19dca816ad29d8af07290be
camilocococ@gmail.com <camilocococ@gmail.com>
2023-03-02 14:01:15 +0100

    final dia 02Mar23

--- src/main/java/com/example/API/LibreriaApplication.java
+++ src/main/java/com/example/API/LibreriaApplication.java
@@ -1,13 +1,13 @@
 package com.example.API;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class LibreriaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LibreriaApplication.class, args);
+		//SpringApplication.run(LibreriaApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/example/API/Prueba.java
@@ -0,0 +1,13 @@
+package com.example.API;
+
+public class Prueba {
+	String desString;
+	
+	public Prueba(String desString) {
+		this.desString = desString;
+	}
+	
+	public void mostrar() {
+		System.out.println(this.desString);
+	}
+}
